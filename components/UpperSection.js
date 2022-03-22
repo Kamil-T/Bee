56b4efb22a7a3b6a322be067397ad692b0222c33
Kamil-T <trzaskomakamil@gmail.com>
2022-03-22 15:40:18 +0100

    UpperSection content finished

--- components/UpperSection.js
+++ components/UpperSection.js
@@ -1,9 +1,17 @@
 import Container from '@mui/material/Container'
 import Navbar from './Navbar'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import IconButton from '@mui/material/IconButton'
+import Image from 'next/image'
 import LetItBee from '../images/letitbee.svg'
 import BigBee from '../images/bigbee.png'
-import Image from 'next/image'
+import ArrowY from '../images/ArrowY.svg'
+import Discord from '../images/DiscordIcon.svg'
+import Twitter from '../images/TwitterIcon.svg'
+import Instagram from '../images/InstagramIcon.svg'
 import styled from '../styles/UpperSection.module.css'
 
 const UpperSection = () => {
@@ -21,8 +29,11 @@ const UpperSection = () => {
       <Box
         maxWidth='1530px'
         sx={{
-          width: '1530px',
-          minHeight: '749px',
+          display: 'flex',
+          flexDirection: 'column',
+          flex: '1',
+          alignItems: 'center',
+          maxHeight: '749px',
           background:
             ' linear-gradient(81.29deg, #FFA232 11.63%, #FFD977 80.12%)',
           borderRadius: '32px',
@@ -30,7 +41,113 @@ const UpperSection = () => {
         <div id={styled.letitbee}>
           <Image src={LetItBee} alt='' />
         </div>
+        <Box
+          id={styled.honey}
+          maxWidth='911.7px'
+          sx={{
+            height: '552px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignSelf: 'center',
+            position: 'absolute',
+            marginTop: '52px',
+          }}>
+          <Typography
+            sx={{
+              fontFamily: 'Clash Grotesk',
+              fontSize: '72px',
+              maxHeight: '97px',
+              fontWeight: '700',
+              letterSpacing: '0.005rem',
+              textAlign: 'center',
+            }}>
+            Lorem ipsum
+          </Typography>
+          <Box
+            maxWidth='911.7px'
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100vw',
+              height: '183px',
+              background: '#DB8216',
+              border:
+                '4px solid linear-gradient(180deg, #DB8116 0%, #F6AD0B 100%)',
+              borderRadius: '32px',
+              transform: 'rotate(-2.41deg)',
+            }}>
+            <Typography
+              sx={{
+                marginBottom: '68px',
+                alignSelf: 'center',
+                height: '151px',
+                textAlign: 'center',
+                fontSize: '150.52px',
+                fontWeight: '700',
+                fontFamily: 'Clash Grotesk',
+                color: 'white',
+                transform: 'rotate(2.41deg)',
+                letterSpacing: '0.005rem',
+              }}>
+              Let it BEE!
+            </Typography>
+          </Box>
+          <Button
+            sx={{
+              alignSelf: 'center',
+              width: '207px',
+              height: '56px',
+              fontSize: '16px',
+              fontFamily: 'Satoshi',
+              letterSpacing: '0.01em',
+              color: '#FFDE73',
+              background: '#8A4A00',
+              boxShadow: '0px 8px 16px #E18821',
+              borderRadius: '100px',
+              transform: 'translateY(-15px)',
+            }}>
+            EXPLORE NOW!
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            margin: 'auto 0 30px',
+            width: '100%',
+            paddingX: '37px',
+            justifyContent: 'space-between',
+          }}>
+          <Stack direction='row'>
+            <IconButton aria-label='scroll down'>
+              <Image src={ArrowY} />
+            </IconButton>
+            <Typography
+              sx={{
+                alignSelf: 'center',
+                fontSize: '12px',
+                fontWeight: '600',
+                fontFamily: 'Montserrat',
+                letterSpacing: '0.24em',
+                color: '#6C3900',
+                paddingX: '8px',
+              }}>
+              SCROLL DOWN
+            </Typography>
+          </Stack>
+          <Stack direction='row' justifyContent='space-between' width='196px'>
+            <IconButton aria-label='discord'>
+              <Image src={Discord} />
+            </IconButton>
+            <IconButton aria-label='twitter'>
+              <Image src={Twitter} />
+            </IconButton>
+            <IconButton aria-label='instagram'>
+              <Image src={Instagram} />
+            </IconButton>
+          </Stack>
+        </Box>
       </Box>
+
       <div id={styled.bigBee}>
         <Image src={BigBee} alt='' />
       </div>
