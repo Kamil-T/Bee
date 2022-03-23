1f127328cd93dfded24dabebe7b0735a93e78b6a
Kamil-T <trzaskomakamil@gmail.com>
2022-03-23 22:53:22 +0100

    RoadMap in progresss

---
+++ components/Phase.js
@@ -0,0 +1,61 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+const Phase = ({ text }) => {
+  return (
+    <Box display='flex' flexDirection='column' height='317px'>
+      <Box
+        display='flex'
+        sx={{
+          justifyContent: 'center',
+          background: '#E9A00A',
+          width: '327.8px',
+          height: '76px',
+          borderRadius: '16px',
+          transform: 'matrix(1, -0.03, 0.03, 1, 0, 0)',
+        }}>
+        <Typography
+          sx={{
+            fontFamily: 'Clash Grotesk',
+            fontWeight: '700',
+            fontSize: '32px',
+            lineHeight: '32px',
+            color: '#FFFFFF',
+            transform: 'matrix(1, 0.03, 0, 1, 0, 0)',
+            alignSelf: 'center',
+          }}>
+          Phase {text}
+        </Typography>
+      </Box>
+      <Box maxWidth='341px' marginTop='17px'>
+        <Typography
+          sx={{
+            fontFamily: 'Montserrat',
+            fontWeight: '700',
+            fontSize: '20px',
+            lineHeight: '32px',
+            letterSpacing: '0.01em',
+            color: 'white',
+          }}>
+          Duis egestas rutrum erat vel finibus. Lorem ipsum dolor.
+        </Typography>
+        <Typography
+          sx={{
+            fontFamily: 'Montserrat',
+            fontWeight: '500',
+            fontSize: '14px',
+            lineHeight: '24px',
+            letterSpacing: '0.01em',
+            color: 'white',
+          }}>
+          Duis egestas rutrum erat vel finibus. Lorem ipsum dolor. Donec dapibus
+          purus eget nisl efficitur, at porttitor ex accumsan. Duis egestas
+          rutrum erat vel finibus. Lorem ipsum dolor. Donec dapibus purus eget
+          nisl efficitur, at porttitor ex accumsan.
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
+
+export default Phase
--- components/RoadMap.js
+++ components/RoadMap.js
@@ -1,11 +1,55 @@
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Phase from './Phase'
+import Image from 'next/image'
+import roadMap from '../images/RoadMap.svg'
 
 const RoadMap = () => {
   return (
     <Container
       id='roadMap'
       maxWidth='false'
-      sx={{ minHeight: '1254px', background: '#DB8116' }}></Container>
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        minHeight: '1254px',
+        paddingTop: '96px',
+        background: '#DB8116',
+      }}>
+      <Box display='flex' flexDirection='column'>
+        <Phase text={'1'} />
+        <Phase text={'3'} />
+      </Box>
+      <Box>
+        <Typography
+          sx={{
+            fontFamily: 'Clash Grotesk',
+            fontWeight: '600',
+            fontSize: '24px',
+            lineHeight: '24px',
+            color: '#8A4A00',
+          }}>
+          Lorem ipsum dolor sit amet
+        </Typography>
+        <Typography
+          sx={{
+            fontFamily: 'Clash Grotesk',
+            fontWeight: '700',
+            fontSize: '64px',
+            lineHeight: '64px',
+            color: 'white',
+          }}>
+          Road map
+        </Typography>
+        <Box alignSelf='center'>
+          <Image src={roadMap} />
+        </Box>
+      </Box>
+      <Box display='flex'>
+        <Phase text={'2'} />
+      </Box>
+    </Container>
   )
 }
 
